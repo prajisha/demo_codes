a1848e7490c8c434ee9fa3439b912b1073fee52b
Prajisha K <prajisha.k@corp.amt.in>
2019-02-19 09:50:26 +0530

    'home'

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+var app = express();
+
+
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine' , 'hbs');
+
+
+app.use( (req,res,next) => {
+	var now = new Date().toString();
+	var log =`${now} : ${req.method} ${req.url}`;
+    console.log(log);
+	fs.appendFile('server.log',log + '\n' , (err)=> {
+		if(err){
+			console.log('unable to update');
+		}
+	});
+	next();
+});
+
+//app.use( (req,res,next) => {
+	//res.render('maintenance.hbs');
+//})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+})
+
+hbs.registerHelper('screamIt' , (text) => {
+	return text.toUpperCase(); 
+})
+
+app.get('/', (req,res) =>{
+res.render('home.hbs' , {
+	pageTitle : 'Home Page',
+	displayMessage : 'Welcome'
+});
+});
+
+app.get('/about' , (req,res) =>{
+	
+	res.render('about.hbs' , {
+		pageTitle : 'About Page'
+	});
+});
+
+
+app.get('/bad', (req,res) => {
+	res.send({
+		errormessage : 'unable to process request'
+	});
+});
+
+app.listen(3000);
